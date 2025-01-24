104dddfb512405a003cf2820bcb7a920d8daa51c
GeometricParrot <98898757+GeometricParrot@users.noreply.github.com>
2025-01-24 16:32:55 -0600

    hello file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+
 fn main() {
-    println!("Hello, world!");
+	let image_width = 256;
+	let image_height = 256;
+
+	println!("P3\n{} {}\n255", image_width, image_height);
+
+	for y in 0..image_height {
+		for x in 0..image_width{
+			let r = x as f32 / (image_width - 1) as f32;
+			let g = (y as f32) / (image_height - 1) as f32;
+			let b = 0.0 as f32;
+
+			let r = (r * 255.999) as i32;
+			let g = (g * 255.999) as i32;
+			let b = (b * 255.999) as i32;
+
+			println!("{} {} {}", r, g, g);
+		}
+	}
 }
